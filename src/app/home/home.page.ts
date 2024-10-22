0b0752d2312375e2a05d42903f8c0e24991c9b2e
Brandy Carney <brandyscarney@gmail.com>
2024-10-22 15:51:19 -0400

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,47 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  @ViewChild(IonModal) modal!: IonModal;
+
+  actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Share',
+      data: {
+        action: 'share',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+
+  alertButtons = ['Action'];
+
+  name!: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+}
